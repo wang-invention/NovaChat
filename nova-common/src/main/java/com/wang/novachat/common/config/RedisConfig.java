830f1c44c714ec79e0c09d84e24a89d8505664ad
root <3346885032@qq.com>
2026-05-14 22:10:47 +0800

    add(messsage):修改部署问题5555

--- nova-common/src/main/java/com/wang/novachat/common/config/RedisConfig.java
+++ nova-common/src/main/java/com/wang/novachat/common/config/RedisConfig.java
@@ -15,6 +15,8 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+
     @Bean
     public RedisTemplate<String, Object> redisTemplate(
             StringRedisTemplate stringRedisTemplate) {
