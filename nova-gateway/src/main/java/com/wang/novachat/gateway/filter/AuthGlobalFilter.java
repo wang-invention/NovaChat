7fcab881beade99e8a011bf0b37be6d0192e45b2
root <3346885032@qq.com>
2026-05-16 11:31:48 +0800

    add(messsage):修改部署问题12313

--- nova-gateway/src/main/java/com/wang/novachat/gateway/filter/AuthGlobalFilter.java
+++ nova-gateway/src/main/java/com/wang/novachat/gateway/filter/AuthGlobalFilter.java
@@ -35,6 +35,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AuthGlobalFilter implements GlobalFilter, Ordered {
 
+
     private static final AntPathMatcher PATH_MATCHER = new AntPathMatcher();
 
     private final JwtService jwtService;
